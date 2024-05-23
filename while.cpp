717f1deeb0bb6f010b2cadc490c77e142735598e
Nishitha <nishithakumarivenur@gmail.com>
2024-05-24 00:10:03 +0530

    Program to demonstrate the usuage of while loop

---
+++ while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int n;
+cin>>n;
+int i=1;
+int sum=0;
+
+while(i<=n){
+    int no;
+   cin>>no;
+  sum=sum+no;
+   i=i+1;
+}
+cout<<"sum:"<<sum;
+    return 0;
+}
