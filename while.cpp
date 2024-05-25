e36f5879cc041f4ddcc9ed3eb1db11be9036edca
Nishitha <nishithakumarivenur@gmail.com>
2024-05-25 14:37:58 +0530

    New comment

--- while.cpp
+++ while.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main() {
 
 int n;
-cin>>n;
+cin>>n;//Taking n numbers as input
 int i=1;
 int sum=0;
 
-while(i<=n){
+while(i<=n)
+{
     int no;
    cin>>no;
   sum=sum+no;
