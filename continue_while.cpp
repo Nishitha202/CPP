a3455192b5a2a2598a8316687607280793e7edd8
Nishitha <nishithakumarivenur@gmail.com>
2024-05-30 23:14:58 +0530

    new comment

---
+++ continue_while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int i=1;
+
+while(i<=20){
+    if(i%7==0){
+        cout<<"Multiple of 7"<<endl;
+        i=i+1;
+        continue;
+    }
+    cout<<i<<endl;
+    i++;
+    
+}
+
+    return 0;
+}
