a3bd1a8ad90ae16e95c710b9ebc9d3186be89170
Nishitha <nishithakumarivenur@gmail.com>
2024-06-25 00:02:35 +0530

    Array declaration

---
+++ Array2.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+  int n;
+  cin>>n;
+  //n is the size of the array
+  int arr[n];
+  
+   cout<<sizeof(arr)/sizeof(int)<<endl;  
+  
+  
+  for(int i=0;i<=n-1;i++){
+      cin>>arr[i];
+  }
+  
+
+  for(int i=0;i<=n-1;i++){
+      cout<<arr[i]<<endl;
+      }
+    return 0;
+}
